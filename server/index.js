9c077e3363a3532a8c3b4be11142c0d5e135ad9c
Yurii <yurageyvakh@gmail.com>
2020-05-19 12:19:01 +0300

    Unary server API implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const grpc = require('grpc');
 
+const greets = require('../server/protos/greet_pb');
+const service = require('../server/protos/greet_grpc_pb');
+
+function greet(call, callback) {
+    const greeting = new greets.GreetResponse();
+    greeting.setResult("hello" + call.request.getGreeting().getFirstname());
+
+    callback(null, greeting);
+}
+
 function Main() {
     const server = new grpc.Server();
 
+    server.addService(service.GreetServiceService, { greet });
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
     server.start();
 
